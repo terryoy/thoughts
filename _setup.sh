36674123a40fbeaf8b5c3f1b612eb5df358dd91e
terryoy <terry.ouyang@gmail.com>
2021-08-30 18:05:07 +0800

    enhance: package setup script

--- _setup.sh
+++ _setup.sh
@@ -2,7 +2,16 @@
 
 
 echo if you want to setup with RubyGems mirror, try:
-echo https://gems.ruby-china.comhi/
+echo https://gems.ruby-china.com/
+echo
+echo "gem sources --add https://gems.ruby-china.com/ --remove https://rubygems.org/"
+echo "gem sources -l"
+echo 
+
+echo "if you have an older version of ruby and bundler"
+echo "sudo apt install ruby2.5 ruby2.5-dev"
+echo "sudo gem install bundler"
+echo
 
 gem install bundler jekyll
 
